ceaedc6d901d958987d30500ab6adb5a237f6f08
Jiyoon Kim <jkim@bronxvilleschool.org>
2022-04-11 23:36:23 -0400

    updated selectionsort

---
+++ final/selection.py
@@ -0,0 +1,25 @@
+#selection sort
+def selectionsort(arr):
+    i = 0
+    while (i < len(dataset)):
+        currmax = findmin(arr,0,len(dataset)-i)
+        arr.append(arr[currmax])
+        arr.pop(currmax);
+        print(arr)
+        i+=1
+    return arr
+
+#findmax finds the first index largest number in the array starting from start
+def findmin(arr,start,end):
+    imax = start;
+    i = start+1
+    while (i < end):
+        if (arr[i]<arr[imax]):
+            imax = i
+        i+=1
+    return imax
+
+dataset = [10, 18, 6, 9, 16, 11, 3, 2, 15, 19, 1, 20, 14, 8, 12, 17, 5, 7, 13, 4];
+
+
+print(selectionsort(dataset))
