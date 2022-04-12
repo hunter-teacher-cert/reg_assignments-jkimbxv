28e770f54ea8af0c4b4d3ad949f5c823a48a8032
Jiyoon Kim <jkim@bronxvilleschool.org>
2022-04-11 23:56:50 -0400

    updated bubble sort

--- final/bubble.py
+++ final/bubble.py
@@ -1,3 +1,23 @@
 #bubble sort
 
+def bubblesort(arr):
+    a = 0
+    while (a<len(arr)):
+        i = 0
+        while (i<len(arr)-1-a):
+            if (arr[i+1]<arr[i]):
+                swap(arr,i+1,i)
+            i+=1
+        a+=1
+    return arr
+
+def swap(arr,a,b):
+    newarr = arr#.copy()
+    temp = newarr[a]
+    newarr[a] = newarr[b]
+    newarr[b] = temp
+    return newarr
+
 dataset = [10, 18, 6, 9, 16, 11, 3, 2, 15, 19, 1, 20, 14, 8, 12, 17, 5, 7, 13, 4];
+
+print(bubblesort(dataset))
